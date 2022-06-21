e61aabd3a2908f5e1cb9e8fa37925e1dce09a5a9
My Name <borisenko_07@mail.ru>
2022-06-21 22:00:35 +0200

    product form added

---
+++ 20_06/scripts/script.js
@@ -0,0 +1,27 @@
+const product_form_elem = document.querySelector('.product_form');
+const products = [];
+
+function render() {
+	const container = document.querySelector('.product_container');
+	container.innerText = ' ';
+	products.forEach(({ title, price }) => {
+		const product = document.createElement('div');
+		const titleElem = document.createElement('p');
+		const priceElem = document.createElement('p');
+		titleElem.innerText = title;
+		priceElem.innerText = price;
+		product.append(titleElem, priceElem);
+		container.append(product);
+	});
+}
+product_form_elem.addEventListener('submit', (event) => {
+	event.preventDefault();
+	const { title, price } = event.target;
+	products.push({
+		title: title.value,
+		price: price.value,
+	});
+	title.value = '';
+	price.value = '';
+	render();
+});
